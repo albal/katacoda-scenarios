f73819d31c3b2950fd058de774a95065c5a13789
Al West <al@tsew.net>
2023-11-04 21:50:44 +0000

    Update verify.sh

--- packer-install/step1/verify.sh
+++ packer-install/step1/verify.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-set -x
+set -e
 gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint | grep "798A EC65 4E5C 1542 8C8E  42EE AA16 FCBC A621 E701"
+apt-cache policy | grep apt.releases.hashicorp.com
